4cdd09798972b9e0b4fa3f0f985bbf242af5bd8b
Andy Kenward <hello@andykenward.com>
2019-05-27 21:31:24 +0100

    chore: update metadata

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
+    title: `Gatsby Default Starter TypeScript`,
     description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    author: `@andykenward`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
